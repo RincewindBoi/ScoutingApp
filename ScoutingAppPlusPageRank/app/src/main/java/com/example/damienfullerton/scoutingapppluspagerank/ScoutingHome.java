80a6c4a0b6dec7ad5eb9312dc1f74e41f3bf4d1b
Damien - Laptop <dvfullerton@gmail.com>
2018-08-10 21:25:56 -0400

    fix it

--- ScoutingAppPlusPageRank/app/src/main/java/com/example/damienfullerton/scoutingapppluspagerank/ScoutingHome.java
+++ ScoutingAppPlusPageRank/app/src/main/java/com/example/damienfullerton/scoutingapppluspagerank/ScoutingHome.java
@@ -59,16 +59,27 @@ public class ScoutingHome extends AppCompatActivity {
                         TeamEntry.COLUMN_NAME_SUBTITLE + "TEXT)";
 
         private static final String SQL_DELETE_ENTRIES =
-                "DROP TABLE IS EXISTS" + TeamEntry.TABLE_NAME;
+                "DROP TABLE IF EXISTS" + TeamEntry.TABLE_NAME;
 
     }
 
-    public class TeamListDbHelper extends SQLiteOpenHelper {
+    public class TeamReaderDbHelper extends SQLiteOpenHelper {
         public static final int DATABASE_VERSION = 1;
-        public static final String DATABASE_NAME = "TeamList.db";
+        public static final String DATABASE_NAME = "TeamReader.db";
 
-        public TeamListDbHelper(Context context) {
-            super(conext, DATABASE_NAME, null, DATABASE_VERSION);
+        public TeamReaderDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newversion){
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
         }
     }
 
